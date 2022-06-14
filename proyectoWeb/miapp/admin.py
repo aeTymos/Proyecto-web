8f43b559889f77052962cb6b77061987adaa2292
aeTymos <vasquezmoralesluis@gmail.com>
2022-06-13 21:13:29 -0400

    validaciones al formulario de agregar

--- proyectoWeb/miapp/admin.py
+++ proyectoWeb/miapp/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Contacto, Region, Ciudad, Comuna, Usuario, Pedido, DetallePedido, Producto, TipoProducto
+from .forms import ProductoForm
 
 #Register your models here.
 class productoAdmin(admin.ModelAdmin):
     list_per_page: 8
+    form = ProductoForm
 
 admin.site.register(Region)
 admin.site.register(Ciudad)
--- proyectoWeb/miapp/forms.py
+++ proyectoWeb/miapp/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Contacto, Producto
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .validators import MaxSizeFileValidator
+from django.forms import ValidationError
 
 class ContactForm(forms.ModelForm):
 
@@ -11,7 +13,22 @@ class ContactForm(forms.ModelForm):
 
 class ProductoForm(forms.ModelForm):
 
+    nombre = forms.CharField(min_length=5, max_length=30)
+    descripcion = forms.CharField(min_length=10, max_length= 80)
+    precio = forms.IntegerField(min_value=1, max_value=200000)
+    miniatura = forms.ImageField(required=False, validators=[MaxSizeFileValidator(max_file_size=2)])
+
+    def clean_nombre(self):
+        nombre = self.cleaned_data['nombre']
+        existe = Producto.objects.filter(nombre__iexact=nombre).exists()
+
+        if existe:
+            raise ValidationError('Ya hay un producto con este nombre')
+
+        return nombre
+
     class Meta:
+
         model = Producto
         fields = '__all__'
 
